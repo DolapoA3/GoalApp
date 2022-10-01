76dece1e496d74bac820427db2602e0754f549b1
LeoKing33 <lapo8387@gmail.com>
2022-09-30 19:49:12 -0500

    Goal Components

--- App.js
+++ App.js
@@ -1,74 +1,43 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
 
 export default function App() {
-    const [enteredGoalText, setEnteredGoalText] = useState('');
-    const [courseGoals, setCourseGoals] = useState([]);
+  const [courseGoals, setCourseGoals] = useState([]);
 
-    function goalInputHandler(enteredText) {
-        setEnteredGoalText(enteredText);
-    }
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, id: Math.random().toString() },
+    ]);
+  }
 
-    function addGoalHandler() {
-        setCourseGoals((currentCourseGoals) => [
-            ...currentCourseGoals,
-            enteredGoalText,
-        ]);
-    }
-
-    return (
-        <View style={styles.appContainer}>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    style={styles.textInput}
-                    placeholder='Your course goal!'
-                    onChangeText={goalInputHandler}
-                />
-                <Button title='Add Goal' onPress={addGoalHandler} />
-            </View>
-            <View style={styles.goalsContainer}>
-                {courseGoals.map((goal) => (
-                    <View key={goal} style={styles.goalItem}>
-                        <Text style={styles.goalText}>{goal}</Text>
-                    </View>
-                ))}
-            </View>
-        </View>
-    );
+  return (
+    <View style={styles.appContainer}>
+      <GoalInput onAddGoal={addGoalHandler} />
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return <GoalItem text={itemData.item.text} />;
+          }}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    appContainer: {
-        flex: 1,
-        paddingTop: 50,
-        paddingHorizontal: 16,
-    },
-    inputContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: '#cccccc',
-    },
-    textInput: {
-        borderWidth: 1,
-        borderColor: '#cccccc',
-        width: '70%',
-        marginRight: 8,
-        padding: 8,
-    },
-    goalsContainer: {
-        flex: 5,
-    },
-    goalItem: {
-        margin: 8,
-        padding: 8,
-        borderRadius: 6,
-        backgroundColor: '#69bbac',
-    },
-    goalText: {
-        color: 'white',
-    },
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
 });
